f86e710581845b32945fc1aa7dbd0c8d3ce33004
ayolethangulo <anayoleth93@gmail.com>
2021-09-22 09:53:31 -0500

    Avances en cuenta corriente

--- BancoDomainTest/CuentaCorrientes/CuentaCorrienteTest.cs
+++ BancoDomainTest/CuentaCorrientes/CuentaCorrienteTest.cs
@@ -8,39 +8,115 @@ using System.Threading.Tasks;
 
 namespace BancoDomainTest.CuentaCorrientes
 {
+    
     public class CuentaCorrienteTest
     {
         /*
-         HU 3.
-        Como Usuario quiero realizar consignaciones a una cuenta corriente para salvaguardar el dinero.
-        Criterios de Aceptación
+         Escenario: Consignacion inicial incorrecta
+        HU 3. COMO Usuario QUIERO realizar consignaciones a una 
+        cuenta corriente para salvaguardar el dinero.
+        Criterios de Aceptación:
         3.1 La consignación inicial debe ser de mínimo 100 mil pesos.
-        3.2 El valor consignado debe ser adicionado al saldo de la cuenta.
+        DADO 
+        CUANDO va consignar un valor de 99.000
+        ENTONCES El sistema presentará el mensaje. “El valor a consignar es incorrecto”
          */
+
         [Test]
         public void NoPuedoConsignarNoventaMilPesosIniciales()
         {
-            var cuentaCorriente = new CuentaCorriente(numero: "10001", nombre: "Cuenta Ejemplo");
-            decimal valorConsignacion = 100000;
+            var cuentaCorriente = new CuentaCorriente(numero: "10001", nombre: "Cuenta Ejemplo",sobregiro: 1000000, ciudad: "Bogota");
+
+            decimal valorConsignacion = 90000;
             string respuesta = cuentaCorriente.Consignar(valorConsignacion: valorConsignacion, fecha: new DateTime(2020, 2, 1));
+            
+            Assert.AreEqual(0, cuentaCorriente.Movimientos.Count);
+            Assert.AreEqual("El valor a consignar es incorrecto", respuesta);
         }
 
+        /*
+         * Escenario: Retiros a una cuenta corriente
+        HU 4.COMO Usuario QUIERO realizar retiros a una cuenta corriente para salvaguardar el dinero.
+        Criterios de Aceptación:
+        4.1 El valor a retirar se debe descontar del saldo de la cuenta.
+        4.2 El saldo mínimo deberá ser mayor o igual al cupo de sobregiro.
+        4.3 El retiro tendrá un costo del 4×Mil
+         */
+
+        [Test]
+        public void PuedoRetirarCienMilIniciales4xMILSobregiro()
+        {
+            #region Dado el cliente tiene una cuenta corriente con un sobregiro permitido de 1.000.000
+            var cuentaCorriente = new CuentaCorriente(numero: "10001", nombre: "Cuenta Ejemplo", sobregiro: 1000000, ciudad: "Bogota");
+            #endregion
+
+            #region Cuando retire 100.000
+            decimal valorRetiro = 100000;
+            string respuesta = cuentaCorriente.Retirar(valorRetiro: valorRetiro, fecha: new DateTime(2020, 2, 1));
+            #endregion
+
+            #region Entonces el sistema descontará el 4xmil de la transacción y el saldo de la cuenta será de 400
+            Assert.AreEqual(0, cuentaCorriente.Movimientos.Count);
+            Assert.AreEqual(-100400, cuentaCorriente.Saldo);
+            Assert.AreEqual("Su Nuevo Saldo es de -100400 pesos m/c", respuesta);
+            #endregion
+        }
     }
 
-    internal class CuentaCorriente
+    public class CuentaCorriente
     {
-        public string Numero;
-        private string Nombre;
+        public string Numero { get; private set; }
+        public string Nombre { get; private set; }
+        public decimal Saldo { get; private set; }
+        public decimal Sobregiro { get; private set; }
+        public string Ciudad { get; private set; }
+        private List<CuentaCorrienteMovimiento> _movimientos;
+        public IReadOnlyCollection<CuentaCorrienteMovimiento> Movimientos => _movimientos.AsReadOnly();
 
-        public CuentaCorriente(string numero, string nombre)
+        public CuentaCorriente(string numero, string nombre, decimal sobregiro, string ciudad)
         {
             this.Numero = numero;
             this.Nombre = nombre;
+            this.Sobregiro = -sobregiro;
+            this.Ciudad = ciudad;
+            _movimientos = new List<CuentaCorrienteMovimiento>();
         }
 
-        internal string Consignar(decimal valorConsignacion, DateTime fecha)
+        public string Consignar(decimal valorConsignacion, DateTime fecha)
         {
+            if (!_movimientos.Any() && valorConsignacion < 100000)
+            {
+                return "El valor a consignar es incorrecto";
+            }
             throw new NotImplementedException();
         }
+
+        internal string Retirar(decimal valorRetiro, DateTime fecha)
+        {
+            var saldoTemporal = Saldo - valorRetiro - valorRetiro * 4 / 1000;
+            if (saldoTemporal > Sobregiro)
+            {
+                Saldo = saldoTemporal;
+                return $"Su Nuevo Saldo es de {Saldo} pesos m/c";
+            }
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CuentaCorrienteMovimiento
+    {
+        public CuentaCorrienteMovimiento(CuentaCorriente cuentaCorriente, DateTime fecha, string tipo, decimal valor)
+        {
+            Cuenta = cuentaCorriente;
+            Fecha = fecha;
+            Tipo = tipo;
+            Valor = valor;
+        }
+
+        public CuentaCorriente Cuenta { get; private set; }
+
+        public DateTime Fecha { get; private set; }
+        public string Tipo { get; private set; }
+        public decimal Valor { get; private set; }
     }
 }
